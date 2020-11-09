9fa0b177631c68e8228280bb75326310ec813f54
Olesia Pashko <pashkoolesia1234@gmail.com>
2020-11-09 19:50:11 +0200

    adds simple food

--- Client/Client/Assets/Scripts/PlayerManager.cs
+++ Client/Client/Assets/Scripts/PlayerManager.cs
@@ -6,18 +6,20 @@ public class PlayerManager : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
+    public int mass = 1;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 direction = (mousePosition - transform.position).normalized;
-            rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
-        }
-        else
-        {
-            rb.velocity = Vector2.zero;
-        }
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - transform.position).normalized;
+        rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+    }
+
+    public void Grow(int mass)
+    {
+        Debug.Log("Grow");
+        this.mass += mass;
+        float scale = 0.1f * mass;
+        transform.localScale += new Vector3(scale, scale, scale);
     }
 }
