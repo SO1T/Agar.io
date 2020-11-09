f7a0c7f01ba646b3fadf6d7697440dd9ea6b6030
Olesia Pashko <pashkoolesia1234@gmail.com>
2020-11-09 19:16:45 +0200

    Add client with ability to move and simple field

---
+++ Client/Client/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePosition - transform.position).normalized;
+            rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
